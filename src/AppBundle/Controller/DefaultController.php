f82acd12c047f461c127ba08cc34518d48c060d4
Adrian Lanzafame <adrianlanzafame92@gmail.com>
2015-08-22 17:39:09 +1000

    Created add tier form and beginnings of the jquery ajax call for the tier data to populate the initial table

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Tier;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,9 +14,20 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
-        ));
+        $pageTitle = "Billing Estimation";
+        $tier1 = new Tier('Tier 1', 1.00, 1000, 1, 1000);
+        $tier2 = new Tier('Tier 2', 0.70, 4000, 1001, 5000);
+        $tier3 = new Tier('Tier 3', 0.50, 10000, 5001, 15000);
+        $tiers = array(
+            "tier1" => $tier1,
+            "tier2" => $tier2,
+            "tier3" => $tier3
+        );
+
+        return $this->render(
+          'form.html.twig',
+          array('tiers' => $tiers,
+                'billing' => $pageTitle)
+        );
     }
 }
--- src/AppBundle/Controller/TierController.php
+++ src/AppBundle/Controller/TierController.php
@@ -2,12 +2,33 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Tier;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class TierController extends Controller
 {
+
+    /**
+    * @Route("/getTiers", name="get_tiers")
+    * @Method("GET")
+    * @Template()
+    */
+    public function viewAction(Request $request)
+    {
+      $response = new JsonResponse(
+          array(
+              'message' => 'Success!'
+          ),
+          200
+      );
+
+      return $response;
+    }
     /**
      * @Route("/newTier")
      * @Template()
