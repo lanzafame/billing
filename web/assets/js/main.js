0f390e1e0355815581bfad8b4bdc08f806c7ea69
Lanzafame <adrianlanzafame92@gmail.com>
2015-08-26 03:06:43 +0000

    The estimate results are now displayed in collapsible tables.

--- web/assets/js/main.js
+++ web/assets/js/main.js
@@ -16,8 +16,6 @@ $.fn.serializeObject = function() {
   return o;
 };
 
-
-
 function getResponse(url, callback) {
   $.getJSON(url, callback);
 }
@@ -30,8 +28,9 @@ function deleteTableRows(callback) {
 function getTiers() {
   getResponse('/getTiers', function(tiers) {
     for (var i = 0; i < tiers.length; i++) {
-      $('#tiertable').append('<tr id="' + tiers[i].id + '"><td>'+tiers[i].id+'<td>'+tiers[i].name+'</td><td>'+tiers[i].price
+      $('#tiertable').append('<tr id="' + tiers[i].id + '"><td>'+tiers[i].id+'</td><td>'+tiers[i].name+'</td><td>'+tiers[i].price
       +'</td><td>'+tiers[i].sizeRange+'</td><td>'+tiers[i].minRange+' - '+tiers[i].maxRange+'</td>');
+	  $('#tier-results-header').append('<th data-field="'+tiers[i].id+'">'+tiers[i].name+'</th>');
     }
   });
 }
@@ -79,20 +78,46 @@ function removeTier() {
 
 function sendClientForm() {
   $('#getestimatebtn').click(function() {
+	var estimate = {};
 	var data = $('form').serializeObject();
 	$.ajax({
 	  method: "POST",
 	  url: "/newClient",
 	  data: data,
+	  async: false,
 	  error: function(xhr, error) {
 		console.debug(xhr); console.debug(error);
 	  },
 	  success: function(response) {
-		console.log("Success");
-		console.log(response);
+		console.log(response.estimate);
+		displayEstimate(response.estimate);
+		displayTierResults(response.estimate);
+		displayCostResults(response.estimate);
+		$('#artefact-results-header').addClass('active');
+		$('#results-list').addClass('active');
+		$('#artefact-results').css("display", "block");
 	  }
 	});
   });
+}
+
+function displayEstimate(json) {
+  $('#artefact-results-table').append('<tr id="' + json.id + '"><td>'+json.total_artefacts+'</td><td>'+json.removed_artefacts+'</td><td>'+json.folded_artefacts+'</td></tr>');
+}
+
+function displayTierResults(json) {
+  $('#tier-results-header').append('<th data-field="du_check_sum">DU Checksum</th>');
+  for (var i = 0; i < $('#tiertable tr').length; i++) {
+	 $('#artefacts-in-tiers').append('<td>'+json.artefacts_in_range[i]+'</td>');
+  }
+  $('#artefacts-in-tiers').append('<td>'+json.du_check_sum+'</td>');
+  for (var i = 0; i < $('#tiertable tr').length; i++) {
+	 $('#price-per-tier-per-month').append('<td>$'+json.price_for_tier_per_month[i]+' / month</td>');
+  }
+}
+
+function displayCostResults(json) {
+  $('#cost-results-table').append('<td>$'+json.price_per_month.toFixed(2)+'</td><td>$'+json.avg_price_per_drawing_per_month.toFixed(2)+'</td><td>$'+json.price_per_annum.toFixed(2)+'</td>');
 }
 
 
